25f06e05d6b0bf6afb1302a68afd0167f99ebf65
Monty <Monty@DESKTOP-QCFM72E>
2022-03-04 20:44:19 -0700

    Lab final

--- src/java/servlets/ShoppingListServlet.java
+++ src/java/servlets/ShoppingListServlet.java
@@ -1,12 +1,13 @@
-
 package servlets;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.ArrayList;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 /**
  *
@@ -14,23 +15,65 @@ import javax.servlet.http.HttpServletResponse;
  */
 public class ShoppingListServlet extends HttpServlet {
 
-    
-   
+    private ArrayList<String> item = new ArrayList<>();
+
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        getServletContext().getRequestDispatcher("/WEB-INF/register.jsp").forward(request, response);
+        HttpSession session = request.getSession();
+        String action = request.getParameter("action");
+
+        String user = (String) session.getAttribute("username");
+        if (action != null && action.equals("Logout")) {
+            session.invalidate();
+            getServletContext().getRequestDispatcher("/WEB-INF/register.jsp").forward(request, response);
             return;
+        }
+        if (user == null) {
+            getServletContext().getRequestDispatcher("/WEB-INF/register.jsp").forward(request, response);
+            return;
+        } else {
+            getServletContext().getRequestDispatcher("/WEB-INF/shoppinglist.jsp").forward(request, response);
+            return;
+        }
+
     }
 
-    
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        getServletContext().getRequestDispatcher("/WEB-INF/register.jsp").forward(request, response);
+        HttpSession session = request.getSession();
+
+        String itemname = request.getParameter("item");
+        String delete = request.getParameter("itemname");
+        String user = request.getParameter("username");
+        String action = request.getParameter("action");
+
+        if (action != null && action.equals("register")) {
+            if (user == null || user.equals("")) {
+                request.setAttribute("errormessage", true);
+                getServletContext().getRequestDispatcher("/WEB-INF/register.jsp").forward(request, response);
+                return;
+            } else {
+                session.setAttribute("username", user);
+                getServletContext().getRequestDispatcher("/WEB-INF/shoppinglist.jsp").forward(request, response);
+                return;
+            }
+        }
+
+        if (action != null && action.equals("add")) {
+            item.add(itemname);
+            session.setAttribute("itemlist", item);
+            getServletContext().getRequestDispatcher("/WEB-INF/shoppinglist.jsp").forward(request, response);
             return;
+        }
+        if (action != null && action.equals("delete")) {
+
+            item.remove(delete);
+            session.setAttribute("itemlist", item);
+            getServletContext().getRequestDispatcher("/WEB-INF/shoppinglist.jsp").forward(request, response);
+            return;
+        }
+
     }
-
-   
-
 }
--- web/WEB-INF/shoppinglist.jsp
+++ web/WEB-INF/shoppinglist.jsp
@@ -4,14 +4,37 @@
     Author     : Monty
 --%>
 
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
 <%@page contentType="text/html" pageEncoding="UTF-8"%>
 <!DOCTYPE html>
 <html>
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-        <title>JSP Page</title>
+        <title>Shopping List-User</title>
     </head>
     <body>
-        <h1>Hello World!</h1>
+        <h1>Shopping List</h1>
+        
+        <p>Hello ${username}   &#x1F60A</p>
+        <a href="<c:url value="/ShoppingList"><c:param name="action" value="Logout"/>
+           </c:url>">Logout</a>
+        
+        <h3>LIST</h3>
+        <form action="ShoppingList" method="post">
+            <label>Add Item:</label>
+            <input type="text" name="item" value="" placeholder="Enter item name">
+            <input type="submit" value="Add">
+             <input type="hidden" name="action" value="add">
+        </form>
+        
+        
+        <form action="ShoppingList" method="post">
+        <c:forEach items="${itemlist}" var="item">
+            <li><input type="radio" name="itemname" value=${item}>${item}</li>
+        </c:forEach><br>
+            <input type="submit" value="Delete">
+             <input type="hidden" name="action" value="delete">
+        </form>
+        
     </body>
 </html>
